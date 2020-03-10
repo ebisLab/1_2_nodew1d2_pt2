9e6b21e13dcc2477c2f93577b3ae5a078da67d00
ebisLab <ebaiden@techie.com>
2020-03-10 19:31:51 -0400

    get request for post

--- data/post/post-router.js
+++ data/post/post-router.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
-const Post = require('../post/post-router');
+const db = require('../db');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.status(200).json({successful: 'Its working!'})
+    // console.log(req, 'req')
+    // res.status(200).json({successful: 'Its working!'})
+    db.find(req.body)
+    .then(post =>{
+        if (post){
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: 'Haw Haw! '})
+
+        }
+    })
 })
 module.exports=router;
